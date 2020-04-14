49e232d7d6b93a327489c6e527fff70f48b66e0c
Xikan1911 <63123522+Xikan1911@users.noreply.github.com>
2020-04-14 04:22:22 -0400

    Add files via upload

---
+++ .github/workflows/animal.py
@@ -0,0 +1,5 @@
+animal = ['dog', 'cat', 'bear', 'wolf', 'tiger', 'lion', 'panda']
+for i in animal:
+    print(i)
+    print("A " + i + " " + "would make a great pet")
+print("Any of these animals would make a great pet" + "!")
---
+++ .github/workflows/pizza.py
@@ -0,0 +1,4 @@
+pizza = ['peperoni', 'four_shiz', 'meat']
+for i in pizza:
+    print("i like " + i + " pizza")    
+print("l really love pizza!")
