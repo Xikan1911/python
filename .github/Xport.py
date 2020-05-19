241d1b2ed634e3e6b194ac2aa7c6c6f728c6df6e
Andrey izmailov <andrey21wow@gmail.com>
2020-05-19 21:37:42 +0300

    Create Xport.py

---
+++ .github/Xport.py
@@ -0,0 +1,8 @@
+favorit_fruits = ['apple', 'orange', 'melon']
+if 'apple' in favorit_fruits:
+    print("Adding apple")
+if 'orange' in favorit_fruits:
+    print("Adding orange")
+if 'qiwi' in favorit_fruits:
+    print("Adding qiwi")
+print("\nFinished making your pizza!")
