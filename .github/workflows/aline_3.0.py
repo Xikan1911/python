7e532be74f01d586c4e495c7d4df351ba39861e5
Andrey izmailov <andrey21wow@gmail.com>
2020-05-19 17:23:01 +0300

    Add files via upload

---
+++ .github/workflows/aline_3.0.py
@@ -0,0 +1,34 @@
+aline_color = 100#If 5 ochkov
+if aline_color >= 100:
+	print("Aline car grenn color")
+	print("You win is 5 ochkov")
+elif aline_color == 50:
+	print("Aline car yellow color")
+	print("You win is 10 ochkov")
+else:
+    print("Aline car red color")
+    print("You win is 15 ochkov")
+
+
+aline_color = 50#Elif 10 ochkov
+if aline_color >= 100:
+	print("Aline car grenn color")
+	print("You win is 5 ochkov")
+elif aline_color == 50:
+	print("Aline car yellow color")
+	print("You win is 10 ochkov")
+else:
+    print("Aline car red color")
+    print("You win is 15 ochkov")
+
+
+aline_color = 0# Else 15 ochkov
+if aline_color >= 100:
+	print("Aline car grenn color")
+	print("You win is 5 ochkov")
+elif aline_color == 50:
+	print("Aline car yellow color")
+	print("You win is 10 ochkov")
+else:
+    print("Aline car red color")
+    print("You win is 15 ochkov")
