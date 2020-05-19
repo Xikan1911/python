9f09f6186b85eb6a1691c9c608924e450c38eae7
Andrey izmailov <andrey21wow@gmail.com>
2020-05-19 21:37:33 +0300

    Create fruct.py

---
+++ .github/fruct.py
@@ -0,0 +1,12 @@
+fruct = ['apple', 'banana', 'orange', 'water_melon', 'melon', 'strawberry']
+if 'apple' in fruct:
+    print("You really like apple")
+if 'banana'in fruct:
+    print("You really like banana")
+if 'orange' in fruct:
+    print("You really like orange")
+if 'water_melon' in fruct:
+    print("You really like water_melon")
+if 'melon' in fruct:
+    print("You really like melon")
+print("\nYou really like other fruct")
