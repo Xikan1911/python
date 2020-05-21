15c92cff34321543d2dc9957f97a81c39dca4f2f
Andrey izmailov <andrey21wow@gmail.com>
2020-05-21 23:08:11 +0300

    Create hello_admin2.py

---
+++ .github/workflows/hello_admin2.py
@@ -0,0 +1,8 @@
+names = ['admin', 'eric', 'franc', 'jonh', 'crack']
+if names:
+    for name in names:
+        print("Hello " + name + '.')
+else:
+    print("We need to find some users!")
+
+print("\nHello admin")
