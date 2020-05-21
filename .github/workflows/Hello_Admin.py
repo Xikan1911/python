c64b78dbbfd88302910c1c6ee12950acd5047bcc
Andrey izmailov <andrey21wow@gmail.com>
2020-05-21 23:00:26 +0300

    Create Hello_Admin.py

---
+++ .github/workflows/Hello_Admin.py
@@ -0,0 +1,5 @@
+names = ['admin', 'eric', 'franc', 'jonh', 'crack']
+for name in names:
+    print("Hello " + name + " thank  you for loggin in again")
+    
+print("\nHello " + 'admin ' + "would you like to see a status report?")
