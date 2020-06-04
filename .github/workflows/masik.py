a52afd7953a8041e665186fe80b668cd2556af20
Xikan1911 <63123522+Xikan1911@users.noreply.github.com>
2020-06-04 22:37:58 +0300

    Create masik.py

---
+++ .github/workflows/masik.py
@@ -0,0 +1,10 @@
+piple = {
+    'fist_name': 'Jura',
+    'last_name': 'Timohin',
+    'city' : 'kolomna',
+    'age': 33
+}
+print(piple['fist_name'])
+print(piple['last_name'])
+print(piple['city'])
+print(piple['age'])
