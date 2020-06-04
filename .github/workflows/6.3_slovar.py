6389f5d3f6d391c48e923fcabba26ddf491b31b9
Xikan1911 <63123522+Xikan1911@users.noreply.github.com>
2020-06-04 22:49:57 +0300

    Create 6.3_slovar.py

---
+++ .github/workflows/6.3_slovar.py
@@ -0,0 +1,7 @@
+Glossarii = {
+    'git': 'tree_update',
+    'bash': 'console',
+    'swift': 'l_program',
+    'python': 'l_program',
+}
+print(*[ [k, v] for k, v in {'git': 'tree_update', 'bash': 'console', 'swift': 'l_program', 'python': 'l_program',}.items() ], sep='\n')
