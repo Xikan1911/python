48f3d44260c75084b2fa25ae446ce72fc9fc3fd3
Xikan1911 <63123522+Xikan1911@users.noreply.github.com>
2020-06-05 01:39:22 +0300

    Update number_spisok.py

--- .github/workflows/number_spisok.py
+++ .github/workflows/number_spisok.py
@@ -5,4 +5,5 @@ number ={
     'sveta': 9,
     'alex':10,
 }
-print(*[ [k, v] for k, v in {'andrey': 2, 'jura': 6, 'victor': 4, 'sveta': 9, 'alex': 10}.items() ], sep='\n')
+for name in number:
+    print(name, number['andrey'])
