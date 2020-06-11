95df9d9582426da722538fce44358abeaa7c4099
Andrey izmailov <andrey21wow@gmail.com>
2020-06-11 14:53:27 +0300

    Create slovari.py

---
+++ .github/workflows/slovari.py
@@ -0,0 +1,13 @@
+Glossarii = {
+    'git': 'tree_update',
+    'bash': 'console',
+    'swift': 'l_program',
+    'python': 'l_program',
+    'print': 'out_txt',
+    'import': 'import_moduls',
+    'powershell': 'shell_windows',
+    'cmd': 'command_shell',
+    'keybooard': 'output_key',
+}
+for name in Glossarii:
+    print(name, Glossarii[name])
