e1c7916cc7d8d20e30f4b1c4da86ff540d3571a5
Andrey izmailov <andrey21wow@gmail.com>
2020-06-11 17:55:14 +0300

    Create 6.6_slowary.py

---
+++ .github/workflows/6.6_slowary.py
@@ -0,0 +1,23 @@
+world = {
+    'oka': 'murom',
+    'nile': 'egypt',
+    'nuke': 'follaut'
+}
+for name, worlds in world.items():
+    print("The " + name.title() + " runs through " + worlds.title() + ".")
+
+world = {
+    'oka': 'murom',
+    'nile': 'egypt',
+    'nuke': 'follaut'
+}
+for v in world.keys():
+    print(v.title())
+
+world = {
+    'oka': 'murom',
+    'nile': 'egypt',
+    'nuke': 'follaut'
+}
+for w in world.values():
+    print(w.title())
