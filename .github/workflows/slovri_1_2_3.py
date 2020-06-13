7514020dd470286f2bb367b338027f34f6cfc24d
Andrey izmailov <andrey21wow@gmail.com>
2020-06-14 00:18:43 +0300

    Update slovri_1_2_3.py

--- .github/workflows/slovri_1_2_3.py
+++ .github/workflows/slovri_1_2_3.py
@@ -1,16 +1,28 @@
-aliens = []
-for aline_number in range(0,30):
-    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
-    aliens.append(new_alien)
-for alien in aliens[0:3]:
-    if alien['color'] == 'green':
-        alien['color'] = 'yellow'
-        alien['speed'] = 'nedium'
-        alien['points'] = 10
-    elif alien['color'] == 'yellow':
-        alien['color'] = 'red'
-        alien['speed'] = 'fast'
-        alien['points'] = 15
-for alien in aliens[:10]:
-    print(alien)
-    print("....")
+people = {
+    'andrey': {
+        'first': 'andrey',
+        'last': 'izmailov',
+        'location': 'kolomna',
+        'age': 20,
+    },
+    'jura': {
+        'first': 'jura',
+        'last': 'Timohin',
+        'location': 'kolomna',
+        'age': 33,
+    },
+    'sveta': {
+        'first': 'Svetlana',
+        'last': 'Izmailova',
+        'location': 'murom',
+        'age': 43,
+    },
+}
+for username, user_info in people.items():
+    print("\nusername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    age = user_info['age']
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
+    print("\tAge: " + str(age))
