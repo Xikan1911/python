55491d969c66baca2f3cd6e2664bd884c0651952
Andrey izmailov <andrey21wow@gmail.com>
2020-10-19 23:32:33 +0300

    master

--- index.py
+++ index.py
@@ -1,2 +1,1 @@
-message = input("You name ?")
-print(message)
+f = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
---
+++ teste.py
@@ -0,0 +1,5 @@
+def greet_user():
+    """Выводит простое приветсвие."""
+    print("Hello")
+
+greet_user()
