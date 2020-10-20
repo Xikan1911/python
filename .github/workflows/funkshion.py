2515a6a577103707d2081455f0998e0ac756155e
Andrey izmailov <andrey21wow@gmail.com>
2020-10-20 20:41:04 +0300

    master

---
+++ .github/workflows/funkshion.py
@@ -0,0 +1,13 @@
+#8.1
+def display_message():
+    """input is easy message"""
+    print("Hello i love the book is piter")
+
+display_message()
+
+#8.2
+def favorite_book(book):
+    """input is my favorite book message"""
+    print(f"One of my favorite books is Alice in Wonderland {book.title()}!")
+
+favorite_book('Python 3 ')
