773edf4dc5e71ac080c66e886e0efe8925b2413c
Andrey izmailov <andrey21wow@gmail.com>
2020-11-18 00:51:52 +0300

    master

--- function II build.py
+++ function II build.py
@@ -8,7 +8,6 @@ make_shirt('52' , 'I love the coding on Python')
 def make_shirt(t_size='L' , t_text='I love python'):
     """output info for t-shitr and size , text"""
     print(f"\nMe need a t-shirt a size {t_size} and text: {t_text}")
-    print(f"\n")
 make_shirt(t_size='L' , t_text='I love python')
 make_shirt('XXL' , 'I love swift')
 #ivent 8.5
